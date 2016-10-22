2081bf459754791cd7a40cfd416f629282897dd4
Sami Aibeche <samiaibeche@gmail.com>
2016-10-22 19:01:54 +0200

    Add Action - WIP Error Redirection Template - Ok Style - WIP

--- server.js
+++ server.js
@@ -1,6 +1,84 @@
-var http = require('http');
+// Main Modules Initialization
+var http    = require('http');
+var mysql   = require('mysql');
+var express = require('express');
+var app     = express();
+var path = require('path');
+var qs = require('querystring');
 
-var server = http.createServer(function(req, res) {
-   console.log('it\'s Work !');
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+
+//Database Create Connection
+var db = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : 'root',
+   database : 'node_todo'
 });
-server.listen(8080);
+
+//Connection - Database
+db.connect();
+
+//Main route
+app.get('/', function (req, res) {
+   db.query('SELECT * FROM tasks ORDER BY created_at DESC', function(err, rows, fields) {
+      if (err)  {
+         res.status(503);
+         res.render("error.ejs", {error : 503});  //503
+      } else {
+         res.render("index.ejs", {tasks: rows});
+      }
+
+   });
+});
+app.post('/addTask', function(request, res) { // Specifies which URL to listen for
+   if (request.method == 'POST') {
+      var body = '';
+      request.on('data', function (data) {
+         body += data;
+         // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+         if (body.length > 1e6) {
+            // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+            request.connection.destroy();
+         }
+      });
+      request.on('end', function () {
+         var task = qs.parse(body);
+         // use POST
+         task = task.taskAdded;
+
+         var d = new Date().toISOString().slice(0, 19).replace('T', ' ');
+         var sql = "INSERT INTO tasks (task, status, created_at) VALUES ('"+task+"', '0', '"+d+"' )";
+         var values = [
+            ['task', task, 1],
+            ['status', 0, 1],
+            ['created_at', d, 3],
+         ];
+
+         db.query(sql, function (err, rows, fields) {
+            if (err) {
+               console.log(err);
+            } else {
+               db.query('SELECT * FROM tasks ORDER BY created_at DESC', function(err, rows, fields) {
+                  if (err)  {
+                     res.status(503);
+                     res.render("error.ejs", {error : 503});  //503
+                  } else {
+                    res.render("index.ejs", {tasks: rows});
+                  }
+
+               });
+            }
+         });
+
+      });
+   }
+});
+//Any other route
+app.get('*', function(req, res){
+   res.status(404);
+   res.render("error.ejs", {error : 404}); //404
+});
+
+app.listen(8181);
