459ec3a6f86b8ebe6726fe72840d4e282e227978
Sami Aibeche <samiaibeche@gmail.com>
2016-10-23 00:03:36 +0200

    Delete Task - WIP

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("form").submit(function(e) {
+    $("#frmAdd").submit(function(e) {
         e.preventDefault(); // Prevents the page from refreshing
         var $this = $(this); // `this` refers to the current form element
         $.post(
@@ -10,6 +10,21 @@ $(document).ready(function(){
         ).done(
             $("body").load(location.href)
 
+        );
+    });
+    $(".delIcon").click(function(e) {
+        e.preventDefault(); // Prevents the page from refreshing
+        var $this = $(this).prev(); // `this` refers to the current form element
+        var id = $($this).attr('id');
+        var task = {idDel : id};
+        
+        $.post(
+           "/deleteTask", // Gets the URL to sent the post to
+            task, // Serializes form data in standard format
+            function(data) { /** code to handle response **/ },
+            "json" // The format the response should be in
+        ).done(
+            $("body").load(location.href)
         );
     });
 });
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var http    = require('http');
 var mysql   = require('mysql');
 var express = require('express');
 var app     = express();
-var path = require('path');
-var qs = require('querystring');
+var path    = require('path');
+var qs      = require('querystring');
 
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
@@ -58,7 +58,8 @@ app.post('/addTask', function(request, res) { // Specifies which URL to listen f
 
          db.query(sql, function (err, rows, fields) {
             if (err) {
-               console.log(err);
+               res.status(503);
+               res.render("error.ejs", {error : 503});  //503
             } else {
                db.query('SELECT * FROM tasks ORDER BY created_at DESC', function(err, rows, fields) {
                   if (err)  {
@@ -75,6 +76,44 @@ app.post('/addTask', function(request, res) { // Specifies which URL to listen f
       });
    }
 });
+
+app.post('/deleteTask', function(request, res) { // Specifies which URL to listen for
+   if (request.method == 'POST') {
+      var body = '';
+      request.on('data', function (data) {
+         body += data;
+         // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+         if (body.length > 1e6) {
+            // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+            request.connection.destroy();
+         }
+      });
+      request.on('end', function () {
+         //Recup data
+         var task = qs.parse(body);
+         //POST
+         var id  = task.idDel;
+         //SQL Request
+         var sql = "DELETE FROM tasks WHERE id ="+id;
+         db.query(sql, function (err, rows, fields) {
+            if (err) {
+               res.status(503);
+               res.render("error.ejs", {error : 503});  //503
+            } else {
+               db.query('SELECT * FROM tasks ORDER BY created_at DESC', function(err, rows, fields) {
+                  if (err)  {
+                     res.status(503);
+                     res.render("error.ejs", {error : 503});  //503
+                  } else {
+                     res.render("index.ejs", {tasks: rows});
+                  }
+               });
+            }
+         });
+      });
+   }
+});
+
 //Any other route
 app.get('*', function(req, res){
    res.status(404);
